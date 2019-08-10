2de1f8d74d66490430a514cd5e1917e81e9fb35c
Derrick Plotsky <derrickplotsky@gmail.com>
2019-08-09 21:44:10 -0700

    Port InvertedIndex
    I am not 100% sure how this is used yet, but I'm sure I will as I get
    used to this library and what's going on.
    
    But from what I can tell, this breaks down tokens into their different
    characters and stores them in an object.
    
    The main things I changed were the addToken function taking in a
    possible "root" as this was never used in the original code ever, and I
    wasn't sure how someone would use that. Maybe later I'll add it back.
    
    I also updated all the refs to just be strings instead of occasionally
    numbers, because they aren't stored that way in the object anyways, so
    it's easier to just have them be strings all over the place.

---
+++ src/InvertedIndex.ts
@@ -0,0 +1,240 @@
+export interface Root {
+  [key: string]: any;
+  docs: { [key: string]: DocInfo };
+  /**
+   * @asType integer
+   */
+  df: number;
+}
+
+export interface SerializedData {
+  root: Root;
+}
+
+export interface DocInfo {
+  tf: number;
+}
+
+export interface TokenInfo extends DocInfo {
+  ref: string;
+}
+
+/**
+ * elasticlunr.InvertedIndex is used for efficiently storing and
+ * lookup of documents that contain a given token.
+ */
+export class InvertedIndex {
+  private _root: Root = {
+    docs: {},
+    df: 0
+  };
+
+  static load(serializedData: SerializedData): InvertedIndex {
+    const index = new InvertedIndex();
+    index._root = serializedData.root;
+    return index;
+  }
+
+  get root(): Root {
+    return this._root;
+  }
+
+  /**
+   * Adds a {token: tokenInfo} pair to the inverted index.
+   * If the token already exists, it updates the tokenInfo.
+   *
+   * tokenInfo format: { ref: 1, tf: 2}
+   * tokenInfor should contains the document's ref and the tf(token frequency) of that token in
+   * the document.
+   *
+   * By default this function starts at the root of the current inverted index, however
+   * it can start at any node of the inverted index if required.
+   *
+   * @param {String} token
+   * @param {Object} tokenInfo format: { ref: 1, tf: 2}
+   * @param {Object} root An optional node at which to start looking for the
+   * correct place to enter the doc, by default the root of this elasticlunr.InvertedIndex
+   * is used.
+   */
+  addToken(token: string, tokenInfo: TokenInfo): void {
+    let root = this._root;
+    let index = 0;
+
+    while (index <= token.length - 1) {
+      const key = token[index];
+
+      if (!Object.keys(root).includes(key)) {
+        root[key] = { docs: {}, df: 0 };
+      }
+      index += 1;
+      root = root[key];
+    }
+
+    const docRef = tokenInfo.ref;
+    if (!root.docs[docRef]) {
+      // if this doc does not exist, then add this doc
+      root.docs[docRef] = { tf: tokenInfo.tf };
+      root.df += 1;
+    } else {
+      // if this doc already exists, then update tokenInfo
+      root.docs[docRef] = { tf: tokenInfo.tf };
+    }
+  }
+
+  /**
+   * Checks whether a token is in this elasticlunr.InvertedIndex.
+   *
+   * @param {String} token The token to be checked
+   * @return {Boolean}
+   */
+  hasToken(token: string): boolean {
+    if (!token) return false;
+
+    let node = this._root;
+
+    for (const char of token) {
+      if (!node[char]) {
+        return false;
+      }
+      node = node[char];
+    }
+
+    return true;
+  }
+
+  /**
+   * Retrieve a node from the inverted index for a given token.
+   * If token not found in this InvertedIndex, return null.
+   *
+   * @param {String} token The token to get the node for.
+   * @return {any}
+   */
+  getNode(token: string): any {
+    if (!token) return;
+
+    let node = this._root;
+
+    for (const char of token) {
+      if (!node[char]) {
+        return;
+      }
+      node = node[char];
+    }
+
+    return node;
+  }
+
+  /**
+   * Retrieve the documents of a given token.
+   * If token not found, return {}.
+   *
+   * @param {String} token The token to get the documents for.
+   * @return {any}
+   */
+  getDocs(token: string): any {
+    const node = this.getNode(token);
+    if (!node) {
+      return {};
+    }
+
+    return node.docs;
+  }
+
+  /**
+   * Retrieve term frequency of given token in given docRef.
+   * If token or docRef not found, return 0.
+   *
+   * @param {String} token The token to get the documents for.
+   * @param {String} docRef
+   * @return {Integer}
+   */
+  getTermFrequency(token: string, docRef: string): number {
+    const node = this.getNode(token);
+
+    if (!node) {
+      return 0;
+    }
+
+    if (!Object.keys(node.docs).includes(docRef)) {
+      return 0;
+    }
+
+    return node.docs[docRef].tf;
+  }
+
+  /**
+   * Retrieve the document frequency of given token.
+   * If token not found, return 0.
+   *
+   *
+   * @param {String} token The token to get the documents for.
+   * @return {Object}
+   */
+  getDocFrequency(token: string): number {
+    const node = this.getNode(token);
+
+    if (!node) {
+      return 0;
+    }
+
+    return node.df;
+  }
+
+  removeToken(token: string, ref: string): void {
+    if (!token) {
+      return;
+    }
+
+    const node = this.getNode(token);
+
+    if (!node) {
+      return;
+    }
+
+    if (Object.keys(node.docs).includes(ref)) {
+      delete node.docs[ref];
+      node.df -= 1;
+    }
+  }
+
+  /**
+   * Find all the possible suffixes of given token using tokens currently in the inverted index.
+   * If token not found, return empty Array.
+   *
+   * @param {String} token The token to expand.
+   * @return {Array<string>}
+   */
+  expandToken(token: string, memo?: string[], tokenRoot?: Root): string[] {
+    if (!token) {
+      return [];
+    }
+
+    const root = tokenRoot || this.getNode(token);
+    if (!root) {
+      return memo || [];
+    }
+
+    const _memo = memo || [];
+    if (root.df > 0) {
+      _memo.push(token);
+    }
+
+    for (const key of Object.keys(root)) {
+      if (key === "docs") {
+        continue;
+      }
+      if (key === "df") {
+        continue;
+      }
+      this.expandToken(token + key, _memo, root[key]);
+    }
+
+    return _memo;
+  }
+
+  toJSON(): { root: Root } {
+    return {
+      root: this._root
+    };
+  }
+}
---
+++ src/__tests__/InvertedIndex.spec.ts
@@ -0,0 +1,397 @@
+import { InvertedIndex, TokenInfo, SerializedData } from "../InvertedIndex";
+
+function buildTokenInfo(ref: string, tf: number): TokenInfo {
+  return { ref, tf };
+}
+
+describe("InvertedIndex", () => {
+  it("constructs empty index", () => {
+    let index = new InvertedIndex();
+    expect(index.root).toEqual({ docs: {}, df: 0 });
+    expect(index.root.df).toEqual(0);
+  });
+
+  describe("addToken()", () => {
+    it("can index basic token", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo);
+      expect(index.root["f"]["o"]["o"]["docs"]["123"]).toEqual({ tf: 1 });
+      expect(index.getDocFrequency("foo")).toEqual(1);
+      expect(index.getTermFrequency("foo", "123")).toEqual(1);
+    });
+
+    it("can add another document to the token successfully", () => {
+      const index = new InvertedIndex();
+      const tokenInfo1 = buildTokenInfo("123", 1);
+      const tokenInfo2 = buildTokenInfo("456", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo1);
+      index.addToken(token, tokenInfo2);
+
+      expect(index.root["f"]["o"]["o"]["docs"]["123"]).toEqual({ tf: 1 });
+      expect(index.root["f"]["o"]["o"]["docs"]["456"]).toEqual({ tf: 1 });
+    });
+
+    it("can add to an existing token", () => {
+      const index = new InvertedIndex();
+      const tokenInfo1 = buildTokenInfo("123", 1);
+      const tokenInfo2 = buildTokenInfo("456", 1);
+      const tokenInfoReplacement = buildTokenInfo("456", 100);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo1);
+      index.addToken(token, tokenInfo2);
+      index.addToken(token, tokenInfoReplacement);
+
+      expect(index.root["f"]["o"]["o"]["docs"][456]).toEqual({ tf: 100 });
+    });
+  });
+
+  describe("hasToken()", () => {
+    it("returns false when given an invalid token", () => {
+      const index = new InvertedIndex();
+      expect(index.hasToken("")).toEqual(false);
+    });
+
+    it("can check a token successfully", () => {
+      const index = new InvertedIndex();
+      const tokenInfo1 = buildTokenInfo("123", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo1);
+
+      expect(index.hasToken(token)).toEqual(true);
+    });
+
+    it("returns false for token that does not exist", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo);
+
+      expect(index.hasToken("fooo")).toEqual(false);
+      expect(index.hasToken("bar")).toEqual(false);
+      expect(index.hasToken("fof")).toEqual(false);
+    });
+
+    it("returns correctly for tokens it has", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo);
+      expect(index.hasToken(token)).toEqual(true);
+      expect(index.hasToken("fo")).toEqual(true);
+      expect(index.hasToken("f")).toEqual(true);
+
+      expect(index.hasToken("bar")).toEqual(false);
+      expect(index.hasToken("foo ")).toEqual(false);
+      expect(index.hasToken("foo  ")).toEqual(false);
+    });
+  });
+
+  describe("getTermFrequency()", () => {
+    it("returns 0 for token that gives no node", () => {
+      const index = new InvertedIndex();
+
+      expect(index.getTermFrequency("foo", "123")).toEqual(0);
+    });
+
+    it("returns 0 when it cannot find docRef", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo);
+
+      expect(index.getTermFrequency("foo", "456")).toEqual(0);
+    });
+
+    it("returns right for single doc", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo);
+
+      expect(index.getTermFrequency("foo", "123")).toEqual(1);
+    });
+
+    it("gets the term frequency with multiple documents", () => {
+      const index = new InvertedIndex();
+      const tokenInfo1 = buildTokenInfo("123", 1);
+      const tokenInfo2 = buildTokenInfo("456", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo1);
+      index.addToken(token, tokenInfo2);
+
+      expect(index.getTermFrequency("foo", "123")).toEqual(1);
+      expect(index.getTermFrequency("foo", "456")).toEqual(1);
+    });
+
+    it("gets correctly from a token that was added to", () => {
+      const index = new InvertedIndex();
+      const tokenInfo1 = buildTokenInfo("123", 1);
+      const tokenInfo2 = buildTokenInfo("456", 1);
+      const tokenInfoReplacement = buildTokenInfo("456", 100);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo1);
+      index.addToken(token, tokenInfo2);
+      index.addToken(token, tokenInfoReplacement);
+
+      expect(index.getTermFrequency("foo", "456")).toEqual(100);
+    });
+  });
+
+  describe("getDocFrequency()", () => {
+    it("can index basic token", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo);
+
+      expect(index.getDocFrequency("foo")).toEqual(1);
+    });
+
+    it("gets the frequency with multiple documents", () => {
+      const index = new InvertedIndex();
+      const tokenInfo1 = buildTokenInfo("123", 1);
+      const tokenInfo2 = buildTokenInfo("456", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo1);
+      index.addToken(token, tokenInfo2);
+
+      expect(index.getDocFrequency("foo")).toEqual(2);
+    });
+
+    it("returns 0 for non-existent token", () => {
+      const index = new InvertedIndex();
+      const tokenInfo1 = buildTokenInfo("123", 1);
+      const tokenInfo2 = buildTokenInfo("456", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo1);
+      index.addToken(token, tokenInfo2);
+
+      expect(index.getDocFrequency(token)).toEqual(2);
+      expect(index.getDocFrequency("fox")).toEqual(0);
+    });
+
+    it("gets correctly from a token that was added to", () => {
+      const index = new InvertedIndex();
+      const tokenInfo1 = buildTokenInfo("123", 1);
+      const tokenInfo2 = buildTokenInfo("456", 1);
+      const tokenInfoReplacement = buildTokenInfo("456", 100);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo1);
+      index.addToken(token, tokenInfo2);
+      index.addToken(token, tokenInfoReplacement);
+
+      expect(index.getDocFrequency(token)).toEqual(2);
+    });
+  });
+
+  describe("getDocs()", () => {
+    it("returns empty for doc that does not exist", () => {
+      const index = new InvertedIndex();
+      expect(index.getDocs("foo")).toEqual({});
+    });
+
+    it("returns empty when giving an empty token", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo);
+      expect(index.getDocs("")).toEqual({});
+    });
+
+    it("returns the doc for a token that has been added", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo);
+      expect(index.getDocs("foo")).toEqual({ "123": { tf: 1 } });
+    });
+
+    it("does not return docs for other tokens", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo);
+      index.addToken("boo", buildTokenInfo("234", 100));
+      index.addToken("too", buildTokenInfo("345", 101));
+
+      expect(index.getDocs("foo")).toEqual({ "123": { tf: 1 } });
+    });
+
+    it("returns all docs for token", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo);
+      index.addToken(token, buildTokenInfo("234", 100));
+      index.addToken(token, buildTokenInfo("345", 101));
+
+      expect(index.getDocs("foo")).toEqual({
+        "123": { tf: 1 },
+        "234": { tf: 100 },
+        "345": { tf: 101 }
+      });
+    });
+  });
+
+  describe("removeToken()", () => {
+    it("removes the token successfully", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo);
+      index.removeToken(token, tokenInfo.ref);
+
+      expect(index.getDocs(token)).toEqual({});
+      expect(index.getDocFrequency(token)).toEqual(0);
+      expect(index.hasToken(token)).toEqual(true);
+    });
+
+    it("removes a non-existant document but does not touch others", () => {
+      const index = new InvertedIndex();
+      const tokenInfo1 = buildTokenInfo("123", 1);
+      const tokenInfo2 = buildTokenInfo("567", 1);
+      const token = "foo";
+
+      index.addToken(token, tokenInfo1);
+      index.addToken("bar", tokenInfo2);
+      index.removeToken(token, "456");
+
+      expect(index.getDocs(token)).toEqual({ 123: { tf: 1 } });
+      expect(index.getDocFrequency("foo")).toEqual(1);
+    });
+
+    it("removes a document from a key that does not exist without exploding", () => {
+      const index = new InvertedIndex();
+
+      index.removeToken("foo", "123");
+      expect(index.hasToken("foo")).toEqual(false);
+      expect(index.getDocFrequency("foo")).toEqual(0);
+    });
+
+    it("does not explode with invalid token", () => {
+      const index = new InvertedIndex();
+      index.removeToken("", "123");
+    });
+  });
+
+  describe("expandToken()", () => {
+    it("returns an empty array when given no token", () => {
+      const index = new InvertedIndex();
+      expect(index.expandToken("")).toEqual([]);
+    });
+
+    it("expands all descendant tokens", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+
+      index.addToken("hell", tokenInfo);
+      index.addToken("hello", tokenInfo);
+      index.addToken("help", tokenInfo);
+      index.addToken("held", tokenInfo);
+      index.addToken("foo", tokenInfo);
+      index.addToken("bar", tokenInfo);
+
+      let tokens = index.expandToken("hel");
+      expect(tokens).toEqual(["hell", "hello", "help", "held"]);
+
+      tokens = index.expandToken("hell");
+      expect(tokens).toEqual(["hell", "hello"]);
+
+      tokens = index.expandToken("he");
+      expect(tokens).toEqual(["hell", "hello", "help", "held"]);
+
+      tokens = index.expandToken("h");
+      expect(tokens).toEqual(["hell", "hello", "help", "held"]);
+    });
+
+    test("expand a non existing token", () => {
+      const index = new InvertedIndex();
+      const tokenInfo = buildTokenInfo("123", 1);
+
+      index.addToken("hell", tokenInfo);
+      index.addToken("hello", tokenInfo);
+      index.addToken("help", tokenInfo);
+      index.addToken("held", tokenInfo);
+      index.addToken("foo", tokenInfo);
+      index.addToken("bar", tokenInfo);
+
+      const tokens = index.expandToken("wax");
+      expect(tokens).toEqual([]);
+    });
+  });
+
+  describe("toJSON()", () => {
+    const index = new InvertedIndex();
+
+    expect(index.toJSON()).toEqual({ root: { docs: {}, df: 0 } });
+
+    index.addToken("foo", buildTokenInfo("123", 1));
+
+    expect(index.toJSON()).toEqual({
+      root: {
+        docs: {},
+        df: 0,
+        f: {
+          df: 0,
+          docs: {},
+          o: {
+            df: 0,
+            docs: {},
+            o: {
+              df: 1,
+              docs: { "123": { tf: 1 } }
+            }
+          }
+        }
+      }
+    });
+  });
+
+  describe("#load()", () => {
+    const serialisedData: SerializedData = {
+      root: {
+        docs: {},
+        df: 0,
+        f: {
+          df: 0,
+          docs: {},
+          o: {
+            df: 0,
+            docs: {},
+            o: {
+              df: 1,
+              docs: { "123": { tf: 1 } }
+            }
+          }
+        }
+      }
+    };
+
+    const index = InvertedIndex.load(serialisedData);
+    const documents = index.getDocs("foo");
+
+    expect(documents).toEqual({ "123": { tf: 1 } });
+  });
+});
